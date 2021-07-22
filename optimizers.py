84edd2390eb776ebb57da31ef3583900a2864133
HongHanh2104 <honghanhlhp1316@gmail.com>
2021-07-22 13:16:43 +0000

    noam optimizer

--- optimizers.py
+++ optimizers.py
@@ -1,3 +1,4 @@
+import numpy as np
 from torch.optim import Adam
 
 
@@ -22,3 +23,30 @@ class NoamOptimizer(Adam):
 
     def lrate(self):
         return self.factor * self.d_model ** (-0.5) * min(self.step_num ** (-0.5), self.step_num * self.warmup_steps ** (-1.5))
+
+
+class ScheduledOptim(Adam):
+    def __init__(self, params, betas, eps, d_model, n_warmup_steps):
+        self.n_warmup_steps = n_warmup_steps
+        self.n_current_steps = 0
+        self.init_lr = np.power(d_model, -0.5)
+        super().__init__(params, lr=self.init_lr, betas=betas, eps=eps)
+
+    def step(self):
+        "Step with the inner optimizer"
+        self._update_learning_rate()
+        super().step()
+
+    def _get_lr_scale(self):
+        return np.min([
+            np.power(self.n_current_steps, -0.5),
+            np.power(self.n_warmup_steps, -1.5) * self.n_current_steps])
+
+    def _update_learning_rate(self):
+        ''' Learning rate scheduling per step '''
+
+        self.n_current_steps += 1
+        lr = self.init_lr * self._get_lr_scale()
+
+        for param_group in self.param_groups:
+            param_group['lr'] = lr
