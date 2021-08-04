71fc9cf6e2cc8f594434e6b8eadb94e94cf64a3f
HongHanh2104 <honghanhlhp1316@gmail.com>
2021-08-04 14:24:55 +0000

    minor changes

--- src/dataset/en_vi_dataset.py
+++ src/dataset/en_vi_dataset.py
@@ -5,100 +5,100 @@ import torch
 from torch.utils import data
 import os
 
-from vncorenlp import VnCoreNLP
+# from vncorenlp import VnCoreNLP
 from transformers import AutoTokenizer
 
 
-class _EN_VIDataset(data.Dataset):
-    def __init__(self, data_dir, phase, vocab_size=None, limit=None, max_len=256):
-        self.data = []
-        self.unk_index = 1
-        self.vocab_size = vocab_size
-        self.limit = limit
-        self.max_len = max_len
+# class _EN_VIDataset(data.Dataset):
+#     def __init__(self, data_dir, phase, vocab_size=None, limit=None, max_len=256):
+#         self.data = []
+#         self.unk_index = 1
+#         self.vocab_size = vocab_size
+#         self.limit = limit
+#         self.max_len = max_len
 
-        def check_index(
-            index): return index if index < vocab_size else self.unk_index
+#         def check_index(
+#             index): return index if index < vocab_size else self.unk_index
 
-        with open(os.path.join(data_dir, f'indexed-{phase}.txt')) as file:
-            for line in file:
-                srcs, trgs = line.strip().split('\t')
-                if vocab_size is not None:
-                    indexed_srcs = [check_index(int(index))
-                                    for index in srcs.strip().split(' ')]
-                    indexed_trgs = [check_index(int(index))
-                                    for index in trgs.strip().split(' ')]
+#         with open(os.path.join(data_dir, f'indexed-{phase}.txt')) as file:
+#             for line in file:
+#                 srcs, trgs = line.strip().split('\t')
+#                 if vocab_size is not None:
+#                     indexed_srcs = [check_index(int(index))
+#                                     for index in srcs.strip().split(' ')]
+#                     indexed_trgs = [check_index(int(index))
+#                                     for index in trgs.strip().split(' ')]
 
-                else:
-                    indexed_srcs = [int(index)
-                                    for index in srcs.strip().split(' ')]
-                    indexed_trgs = [int(index)
-                                    for index in trgs.strip().split(' ')]
+#                 else:
+#                     indexed_srcs = [int(index)
+#                                     for index in srcs.strip().split(' ')]
+#                     indexed_trgs = [int(index)
+#                                     for index in trgs.strip().split(' ')]
 
-                self.data.append((indexed_srcs, indexed_trgs))
-                if limit is not None and len(self.data) > limit:
-                    break
+#                 self.data.append((indexed_srcs, indexed_trgs))
+#                 if limit is not None and len(self.data) > limit:
+#                     break
 
-    def __getitem__(self, item):
-        if self.limit is not None and item >= self.limit:
-            raise IndexError()
+#     def __getitem__(self, item):
+#         if self.limit is not None and item >= self.limit:
+#             raise IndexError()
 
-        indexed_src, indexed_trg = self.data[item]
-        if len(indexed_src) > self.max_len:
-            indexed_src = indexed_src[:self.max_len]
-        if len(indexed_trg) > self.max_len:
-            indexed_trg = indexed_trg[:self.max_len]
+#         indexed_src, indexed_trg = self.data[item]
+#         if len(indexed_src) > self.max_len:
+#             indexed_src = indexed_src[:self.max_len]
+#         if len(indexed_trg) > self.max_len:
+#             indexed_trg = indexed_trg[:self.max_len]
 
-        return indexed_src, indexed_trg
+#         return indexed_src, indexed_trg
 
-    def __len__(self):
-        if self.limit is None:
-            return len(self.data)
-        else:
-            return self.limit
+#     def __len__(self):
+#         if self.limit is None:
+#             return len(self.data)
+#         else:
+#             return self.limit
 
 
-class __EN_VIDataset(data.Dataset):
-    def __init__(self,
-                 src_path,
-                 trg_path,
-                 max_len=256):
-        self.max_len = max_len
+# class __EN_VIDataset(data.Dataset):
+#     def __init__(self,
+#                  src_path,
+#                  trg_path,
+#                  max_len=256):
+#         self.max_len = max_len
 
-        self.src_data = open(src_path).read().splitlines()
-        self.trg_data = open(trg_path).read().splitlines()
+#         self.src_data = open(src_path).read().splitlines()
+#         self.trg_data = open(trg_path).read().splitlines()
 
-        self.rdrsegmenter = VnCoreNLP("../vncorenlp/VnCoreNLP-1.1.1.jar",
-                                      annotators="wseg", max_heap_size='-Xmx500m')
-        self.vi_tokenizer = AutoTokenizer.from_pretrained('vinai/phobert-base')
-        self.en_tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
+#         self.rdrsegmenter = VnCoreNLP("../vncorenlp/VnCoreNLP-1.1.1.jar",
+#                                       annotators="wseg", max_heap_size='-Xmx500m')
+#         self.vi_tokenizer = AutoTokenizer.from_pretrained('vinai/phobert-base')
+#         self.en_tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
 
-    def __getitem__(self, idx):
+#     def __getitem__(self, idx):
 
-        src, trg = self.src_data[idx], self.trg_data[idx]
+#         src, trg = self.src_data[idx], self.trg_data[idx]
 
-        indexed_src = self._tokenize_src_data(src)
-        indexed_trg = self._tokenize_trg_data(trg)
+#         indexed_src = self._tokenize_src_data(src)
+#         indexed_trg = self._tokenize_trg_data(trg)
 
-        return torch.tensor(indexed_src), torch.tensor(indexed_trg)
+#         return torch.tensor(indexed_src), torch.tensor(indexed_trg)
 
-    def __len__(self):
-        return len(self.src_data)
+#     def __len__(self):
+#         return len(self.src_data)
 
-    def _tokenize_trg_data(self, trg_sen):
-        preprocesed_sen = ''
-        sentences = self.rdrsegmenter.tokenize(trg_sen)
-        for sentence in sentences:
-            preprocesed_sen += " ".join(sentence) + ' '
+#     def _tokenize_trg_data(self, trg_sen):
+#         preprocesed_sen = ''
+#         sentences = self.rdrsegmenter.tokenize(trg_sen)
+#         for sentence in sentences:
+#             preprocesed_sen += " ".join(sentence) + ' '
 
-        token = self.vi_tokenizer(preprocesed_sen, padding='max_length',
-                                  truncation=True, max_length=self.max_len)["input_ids"]
-        return token
+#         token = self.vi_tokenizer(preprocesed_sen, padding='max_length',
+#                                   truncation=True, max_length=self.max_len)["input_ids"]
+#         return token
 
-    def _tokenize_src_data(self, src_sen):
-        token = self.en_tokenizer(
-            src_sen, padding='max_length', truncation=True, max_length=self.max_len)["input_ids"]
-        return token
+#     def _tokenize_src_data(self, src_sen):
+#         token = self.en_tokenizer(
+#             src_sen, padding='max_length', truncation=True, max_length=self.max_len)["input_ids"]
+#         return token
 
 
 class EN_VIDataset(data.Dataset):
